46dc4fa362da25ad3463e13c595f41cf6c21c4ba
Azhar Malik <azharmalik02@gmail.com>
2019-01-04 10:10:20 +0500

    Changed version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule NewRelic.Mixfile do
 
   def project do
     [app: :new_relic,
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.7.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
